3da343dc4a70752bf76e30675099c877caa3ed80
Anamika Aggarwal <anamikaagg18@gmail.com>
2025-06-04 21:09:43 +0530

    stack-and-queue

---
+++ stack_queue/queue_to_stack.cpp
@@ -0,0 +1,31 @@
+class MyStack {
+private:
+    queue<int>q;
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        int s = q.size();
+        for(int i=0;i<s-1;i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int el = q.front();
+        q.pop();
+        return el;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
